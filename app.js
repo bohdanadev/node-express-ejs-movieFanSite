47bfa4efdcaec44e22874ba52b5acec500101253
Bohdana K <bohdana.dev@gmail.com>
2024-06-14 01:27:26 +0300

    Add passport  authentication

--- app.js
+++ app.js
@@ -4,13 +4,36 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const session = require('express-session');
+const passport = require('passport');
+const GithubStrategy = require('passport-github').Strategy;
+
 var indexRouter = require('./routes/index');
 
 var app = express();
 const helmet = require('helmet');
 app.use(helmet());
 
-// view engine setup
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+const passportConfig = require('./config')
+passport.use(new GitHubStrategy(passportConfig,
+function(accessToken, refreshToken, profile, cb) {
+  return cb(null, profile);
+}
+));
+passport.serializeUser((user, cb)=>{
+  cb(null,user);
+})
+passport.deserializeUser((user,cb)=>{
+  cb(null,user)
+})
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -22,18 +45,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,25 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
+const passport = require('passport');
+
 
 const apiKey = '1fb720b97cc13e580c2c35e1138f90f8';
+//const apiKey = '123456789';
 const apiBaseUrl = 'http://api.themoviedb.org/3';
+//const apiBaseUrl = 'http://localhost:3030';
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
 const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 router.use((req, res, next) => {
   res.locals.imageBaseUrl = imageBaseUrl;
   next();
-})
+});
 
 router.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
     "default-src 'self'; img-src 'self' data: http://image.tmdb.org http://*.tmdb.org; style-src 'self' 'unsafe-inline' https://maxcdn.bootstrapcdn.com https://fonts.googleapis.com; style-src-elem 'self' 'unsafe-inline' https://maxcdn.bootstrapcdn.com https://fonts.googleapis.com; script-src 'self' https://ajax.googleapis.com https://maxcdn.bootstrapcdn.com; script-src-elem 'self' https://ajax.googleapis.com https://maxcdn.bootstrapcdn.com; font-src 'self' https://fonts.gstatic.com https://maxcdn.bootstrapcdn.com"
-
 
   );
   next();
@@ -24,15 +27,30 @@ router.use((req, res, next) => {
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
- const movieData = await axios.get(nowPlayingUrl).then(value => value.data);
+ try {
+  const movieData = await axios.get(nowPlayingUrl).then(value => value.data);
   res.render('index', {movieData: movieData.results});
+ } catch (e) {
+  console.log(e);
+ }
 });
+
+router.get('/login',passport.authenticate('github'));
+
+router.get('/favorites',(req, res)=>{
+  res.json(req.user.displayName)
+});
+
+router.get('/auth',passport.authenticate('github',{
+  successRedirect: '/',
+  failureRedirect: '/loginFailed'
+}));
 
 router.get('/movie/:id', async function(req, res, next) {
   const movieId = req.params.id;
   const thisMovieUrl = `${apiBaseUrl}/movie/${movieId}?api_key=${apiKey}`
   await axios.get(thisMovieUrl).then((value) => res.render('single-movie', {singleMovie: value.data}))
-})
+});
 
 router.post('/search', async (req, res, next)=>{
   const userSearchTerm = encodeURI(req.body.movieSearch);
@@ -41,7 +59,7 @@ router.post('/search', async (req, res, next)=>{
   const movieData = await axios.get(movieUrl).then(value => value.data);
   res.render('index', {
     movieData: cat==='person' ? movieData.results[0].known_for : movieData.results
-  })
-})
+  });
+});
 
 module.exports = router;
